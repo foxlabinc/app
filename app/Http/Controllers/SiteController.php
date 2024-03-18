782ce70b6069ec401cade38624040324ee763c79
dtischenko <foxlab.inc@gmail.com>
2024-03-18 11:09:16 +0300

    comment

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -17,13 +17,13 @@ class SiteController extends Controller
 	
 	public function test()
 	{
-        $user = User::create([
-            'name' => 'Dmitry',
-            'email' => 'foxlab.inc@gmail.com',
-            'password' => Hash::make('aqswde'),
-        ]);
-        
-        return 'User created';
+        //$user = User::create([
+        //    'name' => 'Dmitry',
+        //    'email' => 'foxlab.inc@gmail.com',
+        //    'password' => Hash::make('aqswde'),
+        //]);
+        //
+        //return 'User created';
 	}
 	
 }
