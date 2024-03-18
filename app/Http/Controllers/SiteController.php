c6ecfc83e4fab5f157cb77cb547fa73f1c1fd79d
dtischenko <foxlab.inc@gmail.com>
2024-03-18 09:40:34 +0300

    start

---
+++ app/Http/Controllers/SiteController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+//use Illuminate\Http\Request;
+//use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class SiteController extends Controller
+{
+    public function index()
+    {
+        //return view('index');
+        return 'index';
+    }
+	
+	public function test()
+	{
+        $user = User::create([
+            'name' => 'Dmitry',
+            'email' => 'foxlab.inc@gmail.com',
+            'password' => Hash::make('aqswde'),
+        ]);
+        
+        return 'User created';
+	}
+	
+}
